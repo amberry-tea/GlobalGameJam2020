3f19a5bdb2c6bbd8742b84425e0711a97bc77e54
Andrew Martin <andrew.martin455@gmail.com>
2020-01-31 22:05:45 -0800

    Add player and test scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Vector3 move;
+    private Vector3 gravity;
+
+    // Start is called before the first frame update
+    void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+
+        move = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown ("d")){
+            move.x = -20;
+        }
+        if(!Input.anyKeyDown){
+            move -= move / 3;
+        }
+        rb.AddForce(move);
+    }
+    /*
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.CompareTag("Collider")) {
+            gravity = Vector3.zero;
+            print(asdwasda);
+        }
+    }*/
+    void OnCollisionEnter(Collision other)
+    {
+         if (other.gameObject.CompareTag("Collider")) {
+            gravity = Vector3.zero;
+            print("asdwasda");
+        }
+    }
+}
