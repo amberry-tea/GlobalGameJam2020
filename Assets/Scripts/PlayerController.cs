7637aacd2a1d12b18a9fa0b11832735bd8c73877
Sean <sean.joseph.green@gmail.com>
2020-02-01 10:39:43 -0800

    JAM collides with the ground but cant move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,38 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;
-    private Vector3 move;
-    private Vector3 gravity;
+    public float speed;
 
     // Start is called before the first frame update
     void Start () {
-        rb = GetComponent<Rigidbody2D> ();
-
-        move = new Vector3(0,0,0);
+        rb = GetComponent<Rigidbody2D> () as Rigidbody2D;
     }
 
-    // Update is called once per frame
-    void Update () {
-        if(Input.GetKeyDown ("d")){
-            move.x = -20;
-        }
-        if(!Input.anyKeyDown){
-            move -= move / 3;
-        }
-        rb.AddForce(move);
+    void fixedUpdate() {
+            float move = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(move*speed, rb.velocity.y);
     }
-    /*
-    void OnTriggerEnter2D (Collider2D other) {
-        if (other.gameObject.CompareTag("Collider")) {
-            gravity = Vector3.zero;
-            print(asdwasda);
-        }
-    }*/
-    void OnCollisionEnter(Collision other)
-    {
-         if (other.gameObject.CompareTag("Collider")) {
-            gravity = Vector3.zero;
-            print("asdwasda");
-        }
-    }
+
 }
