11c70bb42f198e671b6368b516372db8b735f758
Jaedon Braun <jaedonbraun@gmail.com>
2020-02-01 21:38:14 -0800

    SFX Audio Pooling
    - Added 5 AudioSources to player
    - Created SFXplayer script to manage sound effects

---
+++ Assets/Scripts/SfxPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxPlayer : MonoBehaviour
+{
+    public AudioSource[] sourcePool;
+	
+	public void PlaySFX(string sfx){
+		
+		// Runs through every AudioSource in the pool.
+		foreach(AudioSource source in sourcePool){
+			
+			// If this source is not playing sound,
+			// break the loop and play the requested sfx.
+			if(!source.isPlaying){
+				source.PlayOneShot(Resources.Load("SFX/" + sfx) as AudioClip);
+				break;
+			}
+		}
+	}
+}
