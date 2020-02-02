9700c68d89e16fedc29a0760c80f794884d70c6d
Sean <sean.joseph.green@gmail.com>
2020-02-01 17:20:21 -0800

    Player movement with single jump complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,34 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-	
-    private Rigidbody2D rb;
-	private float horiVelocity = 0.0f;	// Horizontal Velocity. Set by player movement.
-	private bool isActive = true;		// Whether or not the player can control JAM.
-    public Animator animator;
-	[Tooltip("JAM's movement speed.")]
-    public float speed;
-	[Tooltip("JAM's jump height.")]
-	public float jumpHeight;
-	[Tooltip("JAM's gravity.")]
-	public float gravity = 9.8f;
-	
+public class PlayerController : MonoBehaviour
+{
 
-    // Start is called before the first frame update
-    void Start () {
-        rb = GetComponent<Rigidbody2D> () as Rigidbody2D;
-    }
+   private Rigidbody2D rb;
+   private float horiVelocity = 0.0f;  // Horizontal Velocity. Set by player movement.
+   public Animator animator;
+   public float speed;
+   public float jumpHeight;
+	public bool hasntJumped;
 
-    void Update() {
-		// sets animator variable
-	    animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
 
-		if(isActive){
-				
-			horiVelocity = Input.GetAxis("Horizontal");
-            
-			rb.velocity = new Vector2(horiVelocity*speed, rb.velocity.y);
-		}
-    }
+   // Start is called before the first frame update
+   void Start()
+   {
+      rb = GetComponent<Rigidbody2D>() as Rigidbody2D;
+   }
+
+   void Update()
+   {
+      // sets animator variable
+      animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+      horiVelocity = Input.GetAxis("Horizontal");
+   }
+
+   void FixedUpdate()
+   {
+      rb.velocity = new Vector2(horiVelocity * speed, rb.velocity.y);
+      if (hasntJumped)
+      {
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+            hasntJumped = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+         }
+      }
+
+   }
+
+	void OnCollisionEnter2D(Collision2D other) {
+		hasntJumped = true;
+
+	}
 }
