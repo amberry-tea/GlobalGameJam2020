4040c6bdd0b55758d06ef4f84ea4822094b201df
Sean <sean.joseph.green@gmail.com>
2020-02-01 16:32:36 -0800

    Merge remote-tracking branch 'origin/master'

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        for(int x = 0; x < map.width; x++)
+        {
+            for(int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile(int x, int y){
+        Color pixelColor = map.GetPixel(x,y);
+
+        if (pixelColor.a == 0)
+        {
+            //Pixel is transparent, thus we ignore it
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if(colorMapping.color.Equals(pixelColor))
+            {    
+                Vector2 position = new Vector2(x,y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+
+}
